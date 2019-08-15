f82bdb1e18209679a138ee29021ed591efd7c408
Fluke667 <Fluke667@gmail.com>
2019-08-15 23:39:51 +0200

    Update adminer.sh

--- config/init/adminer.sh
+++ config/init/adminer.sh
@@ -1,9 +1,57 @@
 #!/bin/sh
 
-mkdir /var/www/adminer &
+mkdir /var/www/adminer /var/log/adminer/ &
 cd /tmp &
 wget https://github.com/vrana/adminer/releases/download/v4.7.2/adminer-4.7.2.php
 cp adminer-4.7.2.php /var/www/adminer
 chown -R nginx /var/www/adminer
+
+cat >/etc/nginx/sites-available/adminer.conf<<-EOF
+server {
+
+    # Listen on port 80
+    listen 80;
+
+    # Server name being used (exact name, wildcards or regular expression)
+    # server_name adminer.my;
+
+    root /var/www/adminer;
+
+    # Logging
+    error_log /var/log/adminer/adminer.access_log;
+    access_log /var/log/adminer/adminer.error_log;
+
+
+   location / {
+           index  index.php;
+       }
+
+       ## Images and static content is treated different
+       location ~* ^.+.(jpg|jpeg|gif|css|png|js|ico|xml)$ {
+           access_log        off;
+           expires           360d;
+       }
+
+       location ~ /\.ht {
+           deny  all;
+       }
+
+       location ~ /(libraries|setup/frames|setup/libs) {
+           deny all;
+           return 404;
+}
+
+    # Pass the PHP scripts to FastCGI server
+    location ~ \.php$ {
+
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME /usr/share/adminer/adminer$fastcgi_script_name;
+        fastcgi_param  HTTPS off;
+    }
+}
+EOF
+
 
 "$@"
