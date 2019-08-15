757d9f9058283d34d54ea55f133c92ca926d87f2
Fluke667 <Fluke667@gmail.com>
2019-08-15 19:30:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,22 @@ cp /config/etc/php7/php.ini /etc/php7/php.ini &
 
 
  
-# if [ -z "${DOCKMAIL}" ]; then
-#    DockLog "ERROR: administrator email is mandatory"
-#  elif [ -z "${DOCKDOMAINS}" ]; then
-#    DockLog "ERROR: at least one domain must be specified"
-#  else
-#    exec certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}" 
-#  fi
-#elif [ "${1}" == 'certbot-renew' ]; then
-#   exec certbot renew
-#else
+ if [ -z "${CERTBOT_EMAIL}" ]; then
+    DockLog "ERROR: administrator email is mandatory"
+  elif [ -z "${CERTBOT_DOMAINS}" ]; then
+    DockLog "ERROR: at least one domain must be specified"
+  else
+    exec certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}"
+  fi
+elif [ "${1}" == 'certbot-renew' ]; then
+   exec certbot renew
+else
+
 
 
 
 #mysql -uroot -p"$MYSQL_ROOT_PASSWORD" test_docker -e "INSERT INTO users(username, name) VALUES ('admin', 'Admin')"
-#/usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 
+mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 
 
 
   "$@"
