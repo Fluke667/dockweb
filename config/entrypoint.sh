8f6807b15fb1fbd90b754f358c11834cd3b6ff6f
Fluke667 <Fluke667@gmail.com>
2019-08-16 00:15:56 +0200

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -15,11 +15,11 @@ ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
 
 
 
-certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}" &
 #mysql -uroot -p"$MYSQL_ROOT_PASSWORD" test_docker -e "INSERT INTO users(username, name) VALUES ('admin', 'Admin')"
 mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 &
 php-fpm7 -c /etc/php7/php.ini -y /etc/php7/php7-fpm.conf &
-nginx
+nginx &
+certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}"
 
 
 
