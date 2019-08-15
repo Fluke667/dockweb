8a97bcdc1d56ce7ca323b7012e2dc3cc1bc217c3
Fluke667 <Fluke667@gmail.com>
2019-08-15 22:19:34 +0200

    Update adminer.sh

--- config/init/adminer.sh
+++ config/init/adminer.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 mkdir /var/www/adminer &
-cd /var/www/adminer &
+cd /tmp &
 wget https://github.com/vrana/adminer/releases/download/v4.7.2/adminer-4.7.2.php
+cp adminer-4.7.2.php /var/www/adminer
 chown -R nginx /var/www/adminer
 
 "$@"
