4f9aaba8934e88db8481292182d88c288e6a9f28
Fluke667 <Fluke667@gmail.com>
2019-08-15 21:52:26 +0200

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -13,22 +13,22 @@ sh /config/init/adminer.sh
  
  if [ -z "${HOST_EMAIL}" ]; then
     DockLog "ERROR: administrator email is mandatory"
-  elif [ -z "${HOST_DOMAINS}" ]; then
+  elif [ -z "${HOST_DOMAIN1}" ]; then
     DockLog "ERROR: at least one domain must be specified"
   else
-    exec /usr/bin/certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAINS}"
+    exec certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}"
   fi
 elif [ "${1}" == 'certbot-renew' ]; then
-   exec /usr/bin/certbot renew
+   exec certbot renew
 else
 
 
 
 
 #mysql -uroot -p"$MYSQL_ROOT_PASSWORD" test_docker -e "INSERT INTO users(username, name) VALUES ('admin', 'Admin')"
-/usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 &
-/usr/sbin/php-fpm7 -c /etc/php7/php.ini -y /etc/php7/php7-fpm.conf &
-/usr/sbin/nginx
+mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 &
+php-fpm7 -c /etc/php7/php.ini -y /etc/php7/php7-fpm.conf &
+nginx
 
 
 
