e7c049bea48edb9d5021291c4dd7b8fb7fda74c7
Fluke667 <Fluke667@gmail.com>
2019-08-16 04:16:10 +0200

    Update domains.sh

--- config/init/domains.sh
+++ config/init/domains.sh
@@ -2,7 +2,45 @@
 
 mkdir -p /etc/nginx/sites-available /etc/nginx/sites-enabled
 
-cat >/etc/nginx/sites-enabled/${HOST_DOMAIN1}.conf<<-EOF
+
+cat >/etc/nginx/sites-enabled/default<<-EOF
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www;
+        index index.php index.html index.htm index.py;
+        server_name _;
+        location / {
+                try_files $uri $uri/ =404;
+        }
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+        }
+}
+# Virtual Host configuration for example.com
+#
+# You can move that to a different file under sites-available/ and symlink that
+# to sites-enabled/ to enable it.
+#
+#server {
+#       listen 80;
+#       listen [::]:80;
+#
+#       server_name example.com;
+#
+#       root /var/www/example.com;
+#       index index.html;
+#
+#       location / {
+#               try_files $uri $uri/ =404;
+#       }
+#}
+EOF
+
+
+
+cat >/etc/nginx/sites-enabled/${HOST_DOMAIN1}.conf<<EOF
 upstream php-handler {
     #server 127.0.0.1:9000;
     server unix:/run/php/php7.2-fpm.sock;
