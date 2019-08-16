fae51050e7d60fb9bd090e43934414b149cdff50
Fluke667 <Fluke667@gmail.com>
2019-08-16 13:15:49 +0200

    Update nginx.sh

--- config/init/nginx.sh
+++ config/init/nginx.sh
@@ -45,7 +45,6 @@ http {
         #
         # Optimized with: https://hostadvice.com/how-to/how-to-tune-and-optimize-performance-of-nginx-web-server/
         ##
-
         sendfile on;
         tcp_nopush on;
         tcp_nodelay on;
@@ -54,7 +53,8 @@ http {
         # server_tokens off;
         client_body_buffer_size 10K;
         client_header_buffer_size 1k;
-        client_max_body_size 8m;
+        client_max_body_size 512M;
+        fastcgi_buffers 64 4K;
         large_client_header_buffers 4 4k;
         client_body_timeout 12;
         client_header_timeout 12;
@@ -70,9 +70,9 @@ http {
         ##
         # SSL Settings
         ##
-
-        ssl_protocols TLSv1 TLSv1.1 TLSv1.2; # Dropping SSLv3, ref: POODLE
-        ssl_prefer_server_ciphers on;
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+        ssl_prefer_server_ciphers off;
 
         ##
         # Logging Settings
@@ -87,12 +87,13 @@ http {
 
         gzip on;
 
-        gzip_vary on;
-        gzip_proxied any;
-        gzip_comp_level 1;
-        gzip_buffers 16 8k;
-        gzip_http_version 1.1;
-        gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+    gzip on;
+    gzip_vary on;
+    gzip_comp_level 4;
+    gzip_min_length 256;
+    gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+    gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+    
 
 
 
