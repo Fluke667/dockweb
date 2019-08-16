594a38c88ded2e3287a78ac9f8d238f962154892
Fluke667 <Fluke667@gmail.com>
2019-08-16 04:55:38 +0200

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-
 cd /config && chmod u+x * -R &
 sh /config/init/mariadb.sh &
 sh /config/init/php7.sh &
 sh /config/init/domains.sh &
-sh /config/init/adminer.sh &
-sh /config/init/openssl.sh &
 sh /config/init/nginx.sh &
+#sh /config/init/adminer.sh &
+#sh /config/init/openssl.sh &
 #cp /config/etc/php7/php.ini /etc/php7/php.ini &
-
-#ln -s /etc/nginx/sites-available/*.conf /etc/nginx/sites-enabled/
-
 
 
 
@@ -20,7 +16,7 @@ sh /config/init/nginx.sh &
 mysqld --user=mysql &
 php-fpm7 -c /etc/php7/php.ini -y /etc/php7/php-fpm.conf &
 nginx &
-certbot certonly --verbose --noninteractive --quiet --standalone --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}"
+certbot certonly --verbose --noninteractive --quiet --nginx --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}, www.${HOST_DOMAIN1}"
 
 
 
