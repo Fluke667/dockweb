7f1360aa1fe3a071ed790b2f1bbc41a090411217
Fluke667 <Fluke667@gmail.com>
2019-08-16 16:50:14 +0200

    Update domains.sh

--- config/init/domains.sh
+++ config/init/domains.sh
@@ -3,7 +3,7 @@
 mkdir -p /etc/nginx/sites-available /etc/nginx/sites-enabled
 
 
-cat >/etc/nginx/sites-enabled/${HOST_DOMAIN1}.conf<<EOF
+cat >/etc/nginx/sites-enabled/${HOST1_DN}.conf<<EOF
 upstream php-handler {
     server unix:/run/php/php7.2-fpm.sock;
 }
@@ -11,7 +11,7 @@ upstream php-handler {
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    server_name $HOST_DOMAIN1;
+    server_name $HOST1_DN;
     autoindex on;
     return 301 https://$host$request_uri;
 }
@@ -19,11 +19,11 @@ server {
 server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
-    server_name $HOST_DOMAIN1;
+    server_name $HOST1_DN;
     autoindex on;
     # certs sent to the client in SERVER HELLO are concatenated in ssl_certificate
-    ssl_certificate /etc/letsencrypt/live/$HOST_DOMAIN1/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$HOST_DOMAIN1/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/$HOST1_DN/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$HOST1_DN/privkey.pem;
     ssl_session_timeout 1d;
     ssl_session_cache shared:MozSSL:10m;  # about 40000 sessions
     ssl_session_tickets off;
@@ -143,7 +143,7 @@ server {
     ssl_stapling_verify on;
 
     # verify chain of trust of OCSP response using Root CA and Intermediate certs
-    ssl_trusted_certificate /etc/letsencrypt/live/$HOST_DOMAIN1/chain.pem;
+    ssl_trusted_certificate /etc/letsencrypt/live/$HOST1_DN/chain.pem;
 
     # replace with the IP address of your resolver
     resolver 1.1.1.1;
