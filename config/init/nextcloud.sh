ad3c65600623a639260e17183c41abb672b1bdef
Fluke667 <Fluke667@gmail.com>
2019-08-22 16:50:31 +0200

    Update nextcloud.sh

--- config/init/nextcloud.sh
+++ config/init/nextcloud.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-cat >/var/www/nextcloud/config/config.php <<-EOF
+
+cat >/usr/bin/occ <<-EOF
+su-exec nginx php -f /var/www/nextcloud/occ 
+EOF
+
+
+#cat >/var/www/nextcloud/config/config.php <<-EOF
 
 
 
