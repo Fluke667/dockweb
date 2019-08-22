0f1311b15a1c540c58a523c44efae94cabe115fe
Fluke667 <Fluke667@gmail.com>
2019-08-22 17:50:56 +0200

    Update nextcloud.sh

--- config/init/nextcloud.sh
+++ config/init/nextcloud.sh
@@ -5,6 +5,10 @@ cat >/usr/bin/occ <<-EOF
 su-exec nginx php -f /var/www/nextcloud/occ 
 EOF
 
+	echo 
+	echo 'Nextcloud init process done. Ready for .....'     
+	echo
+
 
 #cat >/var/www/nextcloud/config/config.php <<-EOF
 
