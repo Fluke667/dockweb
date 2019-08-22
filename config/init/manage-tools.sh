f31d79d756947fc60c7aaa209c8607a55f6b20be
Fluke667 <Fluke667@gmail.com>
2019-08-22 20:25:30 +0200

    Create manage-tools.sh

---
+++ config/init/manage-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget -P /var/www/$HOST2_DN/adminer https://github.com/vrana/adminer/releases/download/v4.7.2/adminer-4.7.2.php
+wget -P /usr/bin https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl
+wget -P /config/db https://github.com/FromDual/mariadb-sys/archive/master.zip
+
+
+chmod +x /usr/bin/mysqltuner.pl
+
+cd /config/db && unzip -x master.zip
+cd /config/db/mariadb-sys-master && mysql -u root -p"$MYSQL_ROOT_PASSWORD" < ./sys_10.sql
