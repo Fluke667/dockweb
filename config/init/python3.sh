0eaacb65a7b551b6218b8c14a7e29cf58cbeb547
Fluke667 <Fluke667@gmail.com>
2019-08-30 21:29:03 +0200

    Update python3.sh

--- config/init/python3.sh
+++ config/init/python3.sh
@@ -6,6 +6,21 @@ virtualenv /home/dockweb/python3 &
 source /home/dockweb/python3/bin/activate &
 pipenv install requests
 
-
+cat >/etc/uwsgi/uwsgi.ini <<-EOF
+[uwsgi]
+socket = /run/uwsgi/uwsgi.sock
+chmod-socket = 775
+chdir = /home/dockweb/python3/apps/data
+master = true
+binary-path = /usr/sbin/uwsgi
+virtualenv = /home/dockweb/python3
+module = flaskapp:app
+uid = www-data
+gid = www-data
+processes = 2
+threads = 4
+plugins = python3,logfile
+logger = file:/var/log/uwsgi.log
+EOF
 
 "$@"
