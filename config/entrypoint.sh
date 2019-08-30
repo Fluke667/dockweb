e327a2a188037d458ffb25fda71e832a4a464fb6
Fluke667 <Fluke667@gmail.com>
2019-08-30 19:43:07 +0200

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,10 +4,15 @@ cd /config && chmod u+x * -R &
 sh /config/pre-install.sh &
 sh /config/init/mariadb.sh &
 sh /config/init/php7.sh &
-sh /config/init/domains.sh &
+sh /config/init/domain1.sh &
+sh /config/init/domain2.sh &
+sh /config/init/domain3.sh &
 sh /config/init/nginx.sh &
 sh /config/init/nextcloud.sh &
+sh /config/init/python3.sh &
 #sh /config/init/openssl.sh &
+sh /config/init/letsencrypt.sh &
+sh /config/init/manage-tools.sh
 #cp /config/etc/php7/php.ini /etc/php7/php.ini &
 
 
@@ -16,7 +21,6 @@ sh /config/init/nextcloud.sh &
 #mysqld --user=mysql &
 #php-fpm7 &
 #nginx &
-#certbot certonly --verbose --noninteractive --quiet --nginx --agree-tos --email="${HOST_EMAIL}" -d "${HOST_DOMAIN1}, www.${HOST_DOMAIN1}"
 #uwsgi --http :8000 --wsgi-file test.py
 
 
