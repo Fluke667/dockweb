a2d93209bb03029525827a6a15de7796ecf76424
Fluke667 <Fluke667@gmail.com>
2019-08-30 19:35:19 +0200

    Update letsencrypt.sh

--- config/init/letsencrypt.sh
+++ config/init/letsencrypt.sh
@@ -15,3 +15,38 @@ echo " ---> Reload Nginx"
 else
   echo "ENTRYPOINT: /etc/letsencrypt/live/$HOST1_DN/fullchain.pem already exists"
         fi
+
+
+if [ ! -f "/etc/letsencrypt/live/$HOST2_DN/fullchain.pem" ]
+        then
+echo " ---> Comment out SSL related directives in configuration"
+   sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-available/$HOST2_DN.conf &
+echo " ---> Reload Nginx"
+   nginx -t && nginx -s reload &
+echo " ---> Obtain Certificate"
+   certbot certonly --verbose --noninteractive --quiet --agree-tos --nginx --force-renewal --webroot /var/www/_letsencrypt -d "${HOST2_DN}, www.${HOST2_DN}" --email="${HOST_EMAIL}" &
+echo " ---> Uncomment SSL related directives in configuration"
+   sed -i -r 's/#?;#//g' /etc/nginx/sites-available/$HOST2_DN.conf &
+echo " ---> Reload Nginx"
+   nginx -t && nginx -s reload
+else
+  echo "ENTRYPOINT: /etc/letsencrypt/live/$HOST2_DN/fullchain.pem already exists"
+        fi
+        
+        
+        
+if [ ! -f "/etc/letsencrypt/live/$HOST3_DN/fullchain.pem" ]
+        then
+echo " ---> Comment out SSL related directives in configuration"
+   sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-available/$HOST3_DN.conf &
+echo " ---> Reload Nginx"
+   nginx -t && nginx -s reload &
+echo " ---> Obtain Certificate"
+   certbot certonly --verbose --noninteractive --quiet --agree-tos --nginx --force-renewal --webroot /var/www/_letsencrypt -d "${HOST3_DN}, www.${HOST3_DN}" --email="${HOST_EMAIL}" &
+echo " ---> Uncomment SSL related directives in configuration"
+   sed -i -r 's/#?;#//g' /etc/nginx/sites-available/$HOST3_DN.conf &
+echo " ---> Reload Nginx"
+   nginx -t && nginx -s reload
+else
+  echo "ENTRYPOINT: /etc/letsencrypt/live/$HOST3_DN/fullchain.pem already exists"
+        fi
