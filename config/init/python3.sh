940f7b403ef23242a43458909d4d6cec8713726e
Fluke667 <Fluke667@gmail.com>
2019-08-30 21:20:42 +0200

    Update python3.sh

--- config/init/python3.sh
+++ config/init/python3.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-virtualenv dockweb
+cd /home/dockweb/python3 &
+pip3 install --upgrade pip gixy virtualenv pipenv &
+virtualenv /home/dockweb/python3 &
+source /home/dockweb/python3/bin/activate &
+pipenv install requests
+
+
+
+"$@"
