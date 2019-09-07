7c34c50fb26499f41880a7b604b6551c77d5c552
Fluke667 <Fluke667@gmail.com>
2019-09-07 04:41:14 +0200

    Update nextcloud.sh

--- config/init/nextcloud.sh
+++ config/init/nextcloud.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-wget -P /var/www/$HOST1_DN ${NEXTCLOUD_DL}.tar.bz2 && cd /var/www/$HOST1_DN && tar -xjf latest.tar.bz2 &
+
+cd /tmp
+wget -q  ${NEXTCLOUD_DL}.tar.bz2 &
+tar -xjf latest.tar.bz2 --strip 1 -C ${NEXTCLOUD_PATH} &
 mkdir -p $NEXTCLOUD_PATH/data $NEXTCLOUD_PATH/assets $NEXTCLOUD_PATH/updater &
 find ${NEXTCLOUD_PATH} -type f -print0 -maxdepth 1 | xargs -0 chmod 0640 &
 find ${NEXTCLOUD_PATH} -type d -print0 -maxdepth 1 | xargs -0 chmod 0750 &
 chown ${ROOTUSR}:${NGINX_WWWUSER} ${NEXTCLOUD_PATH}/. &
 chown ${NGINX_WWWUSR}:${NGINX_WWWGRP} ${NEXTCLOUD_PATH}/data &
-find ${NEXTCLOUD_PATH} ! -path */nextcloud/data/* -print0 | xargs -0 chown -R ${NGINX_WWWUSR}:${NGINX_WWWGRP}
+find ${NEXTCLOUD_PATH} ! -path */nextcloud/data/* -print0 | xargs -0 chown -R ${NGINX_WWWUSR}:${NGINX_WWWGRP} &
 chmod +x ${NEXTCLOUD_PATH}/occ
 
 cat >/usr/bin/occ <<-EOF
@@ -14,8 +17,20 @@ EOF
 
 
 
-#cat >/var/www/nextcloud/config/config.php <<-EOF
-chmod +x /usr/bin/occ
+cat >/var/www/nextcloud/config/autoconfig.php <<-EOF
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "$NEXTCLOUD_DB_TYPE",
+  "dbname"        => "$NEXTCLOUD_DB_DATABASE",
+  "dbuser"        => "$NEXTCLOUD_DB_USER",
+  "dbpass"        => "$NEXTCLOUD_DB_PASS",
+  "dbhost"        => "$NEXTCLOUD_DB_HOST",
+  "dbtableprefix" => "$NEXTCLOUD_DB_PREFIX",
+  "adminlogin"    => "$NEXTCLOUD_USER",
+  "adminpass"     => "$NEXTCLOUD_PASS",
+  "directory"     => "$NEXTCLOUD_DATA",
+);
+EOF
 
 
 
